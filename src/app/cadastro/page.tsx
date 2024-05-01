695cbdf17ca2d1203422ff775d3ca7b916436ee3
Marcelo Anjos <none@email.com>
2024-05-01 10:44:27 -0300

    style(form): adiciona bootstrap para design

--- src/app/cadastro/page.tsx
+++ src/app/cadastro/page.tsx
@@ -1,75 +1,92 @@
+"use client";
+
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import InputGroup from 'react-bootstrap/InputGroup';
+import Button from 'react-bootstrap/Button';
+
+import { insertItem } from './action';
+
 export default function Page() {
-
-    async function insertItem(formData: FormData) {
-        "use server"
-        const obj = {
-            id: formData.get("id"),
-            description: formData.get("description"),
-            quantity: formData.get("quantity"),
-        }
-        console.log(obj);
-    }
-
     return (
-        <form action={insertItem}>
-            <div>
-                <label>
-                    ID: <input type="number" name="id" min={0} />
-                </label>
-            </div>
-            <div>
-                <label>
-                    Descrição: <input type="text" name="nome" />
-                </label>
-            </div>
-            <div>
-                <label>
-                    Cor: <input type="color" name="cor" />
-                </label>
-            </div>
-            <div>
-                <label>
-                    Raio mínimo de dobra dentro do conduite: <input type="number" step='any' name="raio" />
-                </label>
-            </div>
-            <div>
-                <label>
-                    Tipo comercial: <input type="text" name="tipo" />
-                </label>
-            </div>
-            <div>
-                <label>
-                    Unidade da medida da bitola: <input type="number" step='any' name="bitola" />
-                </label>
-            </div>
-            <div>
-                <label>
-                    Preço de compra sem os impostos: <input type="number" step='any' name="price_buy" />
-                </label>
-            </div>
-            <div>
-                <label>
-                    Preço de venda sem os impostos <input type="number" step='any' name="price_liquid_sell" disabled />
-                </label>
-            </div>
-            <div>
-                Preço de venda com os impostos: <input type="number" step='any' name="price_brute_sell" disabled />
-            </div>
-            <div>
-                <label>
-                    Quantidade: <input type="number" name="quantity" /> (metros)
-                </label>
-            </div>
-            <div>
-                <label>
-                    Valor do item em estoque: <input type="number" step='any' name="" />
-                </label>
-            </div>
-            <div>
-                Acumulado do estoque: <input type="number" step='any' name="acumulado" disabled />
-            </div>
-            <input type="reset" value="Limpar" />
-            <input type="submit" value="Cadastrar" />
-        </form>
+        <Container>
+            <Form action={insertItem}>
+                <Form.Group>
+                    <Form.Label>
+                        ID: <Form.Control type="number" name="id" min={0} />
+                    </Form.Label>
+                </Form.Group>
+                <Form.Group>
+                    <Form.Label>
+                        Descrição: <Form.Control type="text" name="nome" />
+                    </Form.Label>
+                </Form.Group>
+                <Form.Group>
+                    <Form.Label>
+                        Cor: <Form.Control type="color" name="cor" />
+                    </Form.Label>
+                </Form.Group>
+                <Form.Group>
+                    <Form.Label>
+                        Raio mínimo de dobra dentro do conduite: <Form.Control type="number" step='any' name="raio" />
+                    </Form.Label>
+                </Form.Group>
+                <Form.Group>
+                    <Form.Label>
+                        Tipo comercial: <Form.Control type="text" name="tipo" />
+                    </Form.Label>
+                </Form.Group>
+                <Form.Group>
+                    <Form.Label>
+                        Unidade da medida da bitola: <Form.Control type="number" step='any' name="bitola" />
+                    </Form.Label>
+                </Form.Group>
+                <Form.Group>
+                    <Form.Label>
+                        Preço de compra sem os impostos: <Form.Control type="number" step='any' name="price_buy" />
+                    </Form.Label>
+                </Form.Group>
+                <Form.Group>
+                    <Form.Label>
+                        Preço de venda sem os impostos
+                        <InputGroup>
+                            <InputGroup.Text>R$</InputGroup.Text><Form.Control type="number" step='any' name="price_liquid_sell" disabled />
+                        </InputGroup>
+                    </Form.Label>
+                </Form.Group>
+                <Form.Group>
+                    <Form.Label>
+                        Preço de venda com os impostos:
+                        <InputGroup>
+                            <InputGroup.Text>R$</InputGroup.Text><Form.Control type="number" step='any' name="price_brute_sell" disabled />
+                        </InputGroup>
+                    </Form.Label>
+                </Form.Group>
+                <Form.Group>
+                    <Form.Label>
+                        Quantidade:
+                        <InputGroup>
+                            <Form.Control type="number" name="quantity" /> <InputGroup.Text>metros</InputGroup.Text>
+                        </InputGroup>
+                    </Form.Label>
+                </Form.Group>
+                <Form.Group>
+                    <Form.Label>
+                        Valor do item em estoque:
+                        <InputGroup>
+                            <InputGroup.Text>R$</InputGroup.Text><Form.Control type="number" step='any' name="" />
+                        </InputGroup>
+                    </Form.Label>
+                </Form.Group>
+                <Form.Group>
+                    <Form.Label>
+                        Acumulado do estoque: <Form.Control type="number" step='any' name="acumulado" disabled />
+                    </Form.Label>
+                </Form.Group>
+                <Button variant='danger' type='reset'>Limpar</Button>
+                <Button variant='warning'>Editar</Button>
+                <Button variant='primary' type='submit'>Cadastrar</Button>
+            </Form>
+        </Container>
     )
 }
